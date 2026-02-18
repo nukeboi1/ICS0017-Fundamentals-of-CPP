89ec75cd99ebcc2398162ce60185dcb6a508dfe1
nukeboi1 <username1023456789@gmail.com>
2026-02-18 14:45:45 +0200

    Ex4 done

---
+++ Practice1_Ex4.cpp
@@ -0,0 +1,12 @@
+// Replace ___ with your code below
+#include <iostream>
+using namespace std;
+int main() {
+	// create two variables
+	double number1 = 9.9;
+	double number2 = 2.2;
+	// add two variables
+	double sum = number1 + number2;
+	cout << sum;
+	return 0;
+}
