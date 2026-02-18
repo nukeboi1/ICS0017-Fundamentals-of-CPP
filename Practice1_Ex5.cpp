2fbc2f5a60c33566541312452e7c4691f55a3d4c
nukeboi1 <username1023456789@gmail.com>
2026-02-18 14:56:04 +0200

    Ex5 done

---
+++ Practice1_Ex5.cpp
@@ -0,0 +1,10 @@
+// Replace ___ with your code below
+#include <iostream>
+using namespace std;
+int main() {
+	// create double variable number and assign 5.6 to it
+	double number = 5.6;
+		// print the square of number
+	cout << number * number;
+		return 0;
+}
