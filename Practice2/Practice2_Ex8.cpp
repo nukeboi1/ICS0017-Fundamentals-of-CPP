1abff2f93bb0ad9d8210b27523c4185c11c7051d
nukeboi1 <username1023456789@gmail.com>
2026-02-18 18:59:50 +0200

    ex8 done

---
+++ Practice2/Practice2_Ex8.cpp
@@ -0,0 +1,11 @@
+// Replace ___ with your code below
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string text = "C++ Programming";
+	for (int i = 0; i < text.length(); ++i) {
+		cout << text[i];
+	}
+	return 0;
+}
