5acb5e8afbcd767c7d8cbea63b2c139e111c0896
nukeboi1 <username1023456789@gmail.com>
2026-02-18 15:57:29 +0200

    ex9 done.

---
+++ Practice1_Ex9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>;
+using namespace std;
+int main() {
+	int number = 9;
+	int result;
+	for (int i = 1; i <= 5; i++) {
+		result = number * i;
+		cout << number << " * " << i << " = " << result << endl;
+	}
+}
