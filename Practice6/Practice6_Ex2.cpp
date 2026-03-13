3e4dbb4fbfbd7094ad85a01c0e698d50078da9d5
nukeboi1 <username1023456789@gmail.com>
2026-03-13 15:55:36 +0200

    practice 6 ex2

---
+++ Practice6/Practice6_Ex2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Greeting {
+public:
+	void greet(const string& name) {
+		cout << "Hello " << name << endl;
+	}
+};
+class Local_Greeting : public Greeting {
+public:
+	void greet(const string& name) {
+		cout << "Namaste " << name << endl;
+	}
+};
+int main() {
+	string name;
+	getline(cin, name);
+	Greeting g1;
+	g1.greet(name);
+	Local_Greeting g2;
+	g2.greet(name);
+	return 0;
+}
