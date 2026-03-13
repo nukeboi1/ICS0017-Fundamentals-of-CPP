d49ca40bf8f4359ed509d63dc1c9262645a7ddda
nukeboi1 <username1023456789@gmail.com>
2026-03-13 16:44:22 +0200

    p6, ex8 done

---
+++ Practice6/Practice6_Ex8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class Polygon {
+	virtual double area() = 0;
+};
+class Trianle : public Polygon {
+public:
+	double base, height;
+	Trianle(double base, double height) : base(base), height(height) {}
+	double area() {
+		return 0.5 * base * height;
+	}
+};
+class Rectangle : public Polygon {
+public:
+	double length, breadth;
+	Rectangle(double l, double b) {
+		length = l;
+		breadth = b;
+	}
+	double area() {
+		return length * breadth;
+	}
+};
+int main() {
+	double base, height, length, breadth;
+	cin >> base >> height >> length >> breadth;
+	Trianle t1(base, height);
+	Rectangle r1(length, breadth);
+	double area_triangle = t1.area();
+	double area_rectangle = r1.area();
+	cout << area_triangle << endl << area_rectangle;
+	return 0;
+}
