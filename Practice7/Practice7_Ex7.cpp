9c60b7fbdd141594b118c106d9843ce6a15e3d1d
nukeboi1 <username1023456789@gmail.com>
2026-03-18 15:30:21 +0200

    p7ex7 done

---
+++ Practice7/Practice7_Ex7.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+class Account {
+public:
+	string account_id;
+protected:
+	string password;
+};
+class Email : public Account {
+public:
+	void set_id(string acc) {
+		account_id = acc;
+	}
+	string get_id() {
+		return account_id;
+	}
+	void set_password(string pw) {
+		password = pw;
+	}
+	string get_password() {
+		return password;
+	}
+};
+int main() {
+	string acc, pw;
+	cin >> acc >> pw;
+	Email email;
+	email.set_id(acc);
+	email.set_password(pw);
+	cout << email.get_id() << endl;
+	cout << email.get_password() << endl;
+	return 0;
+}
